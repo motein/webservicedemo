354eab4f25b9fcbe9fdc551d4fdd6eddc43524a7
motein <motein@outlook.com>
2018-02-05 01:43:38 +0800

    Add simple-service-3 project.

---
+++ simple-service-3/src/main/java/com/example/annotation/param/bean/ParamCache.java
@@ -0,0 +1,104 @@
+package com.example.annotation.param.bean;
+
+import com.example.domain.Yijing;
+
+import java.util.ArrayList;
+
+public class ParamCache {
+    public final static ArrayList<Yijing> LIST;
+
+    static {
+        LIST = new ArrayList<Yijing>();
+        ParamCache.LIST.add(new Yijing("1", "Ç¬", "qi¨¢n"));
+        ParamCache.LIST.add(new Yijing("2", "À¤", "k¨±n"));
+        ParamCache.LIST.add(new Yijing("3", "ÍÍ", "zh¨±n"));
+        ParamCache.LIST.add(new Yijing("4", "ÃÉ", "m¨¦ng"));
+        ParamCache.LIST.add(new Yijing("5", "Ðè", "x¨±"));
+
+        ParamCache.LIST.add(new Yijing("6", "ËÏ", "s¨°ng"));
+        ParamCache.LIST.add(new Yijing("7", "Ê¦", "sh¨©"));
+        ParamCache.LIST.add(new Yijing("8", "±È", "b¨¬"));
+        ParamCache.LIST.add(new Yijing("9", "Ð¡Ðó", "xiao x¨´"));
+        ParamCache.LIST.add(new Yijing("10", "ÂÄ", "l¨·"));
+
+        ParamCache.LIST.add(new Yijing("11", "Ì©", "t¨¤i"));
+        ParamCache.LIST.add(new Yijing("12", "·ñ", "p¨«"));
+        ParamCache.LIST.add(new Yijing("13", "Í¬ÈË", "t¨®ng r¨¦n"));
+        ParamCache.LIST.add(new Yijing("14", "´óÓÐ", "d¨¤ y¨­u"));
+
+        ParamCache.LIST.add(new Yijing("15", "Ç«", "qi¨¡n"));
+        ParamCache.LIST.add(new Yijing("16", "Ô¥", "y¨¸"));
+        ParamCache.LIST.add(new Yijing("17", "Ëæ", "su¨ª"));
+        ParamCache.LIST.add(new Yijing("18", "¹Æ", "g¨³"));
+        ParamCache.LIST.add(new Yijing("19", "ÁÙ", "l¨ªn"));
+
+        ParamCache.LIST.add(new Yijing("20", "¹Û", "gu¨¡n"));
+        ParamCache.LIST.add(new Yijing("21", "ÊÉà¾", "sh¨¬ h¨¦"));
+        ParamCache.LIST.add(new Yijing("22", "êÚ", "b¨¬"));
+        ParamCache.LIST.add(new Yijing("23", "°þ", "b¨­"));
+        ParamCache.LIST.add(new Yijing("24", "¸´", "f¨´"));
+
+        ParamCache.LIST.add(new Yijing("25", "ÎÞÍý", "w¨² w¨¤ng"));
+        ParamCache.LIST.add(new Yijing("26", "´óÐó", "d¨¤ x¨´"));
+        ParamCache.LIST.add(new Yijing("27", "ÒÃ", "y¨ª"));
+        ParamCache.LIST.add(new Yijing("28", "´ó¹ý", "d¨¤ gu¨°"));
+
+        ParamCache.LIST.add(new Yijing("29", "¿²", "k¨£n"));
+        ParamCache.LIST.add(new Yijing("30", "Àë", "l¨ª"));
+        ParamCache.LIST.add(new Yijing("31", "ÏÌ", "xi¨¢n"));
+        ParamCache.LIST.add(new Yijing("32", "ºã", "h¨¦ng"));
+        ParamCache.LIST.add(new Yijing("33", "¶Ý", "d¨´n"));
+
+        ParamCache.LIST.add(new Yijing("34", "´ó×³", "d¨¤ zhu¨¤ng"));
+        ParamCache.LIST.add(new Yijing("35", "½ú", "j¨¬n"));
+        ParamCache.LIST.add(new Yijing("36", "Ã÷ÒÄ", "m¨ªng y¨ª"));
+        ParamCache.LIST.add(new Yijing("37", "¼ÒÈË", "ji¨¡ ren"));
+
+        ParamCache.LIST.add(new Yijing("38", "î¥", "ku¨ª"));
+        ParamCache.LIST.add(new Yijing("39", "å¿", "ji¨£n"));
+        ParamCache.LIST.add(new Yijing("40", "½â", "xi¨¨"));
+        ParamCache.LIST.add(new Yijing("41", "Ëð", "s¨³n"));
+        ParamCache.LIST.add(new Yijing("42", "Òæ", "y¨¬"));
+
+        ParamCache.LIST.add(new Yijing("43", "‰ø", "gu¨¤i"));
+        ParamCache.LIST.add(new Yijing("44", "Š¥", "g¨°u"));
+        ParamCache.LIST.add(new Yijing("45", "ÝÍ", "cu¨¬"));
+        ParamCache.LIST.add(new Yijing("46", "Éý", "sh¨¥ng"));
+        ParamCache.LIST.add(new Yijing("47", "À§", "k¨´n"));
+
+        ParamCache.LIST.add(new Yijing("48", "¾®", "j¨«ng"));
+        ParamCache.LIST.add(new Yijing("49", "¸ï", "g¨¦"));
+        ParamCache.LIST.add(new Yijing("50", "¶¦", "d¨«ng"));
+        ParamCache.LIST.add(new Yijing("51", "Õð", "zh¨¨n"));
+        ParamCache.LIST.add(new Yijing("52", "ôÞ", "g¨¨n"));
+
+        ParamCache.LIST.add(new Yijing("53", "½¥", "ji¨¤n"));
+        ParamCache.LIST.add(new Yijing("54", "¹éÃÃ", "gu¨© m¨¨i"));
+        ParamCache.LIST.add(new Yijing("55", "·á", "f¨¥ng"));
+        ParamCache.LIST.add(new Yijing("56", "ÂÃ", "l¨·"));
+
+        ParamCache.LIST.add(new Yijing("57", "Ùã", "x¨´n"));
+        ParamCache.LIST.add(new Yijing("58", "¶Ò", "du¨¬"));
+        ParamCache.LIST.add(new Yijing("59", "»Á", "hu¨¤n"));
+        ParamCache.LIST.add(new Yijing("60", "½Ú", "ji¨¦"));
+        ParamCache.LIST.add(new Yijing("61", "ÖÐæÚ", "zh¨­ng f¨²"));
+
+        ParamCache.LIST.add(new Yijing("62", "Ð¡¹ý", "xi¨£o gu¨°"));
+        ParamCache.LIST.add(new Yijing("63", "¼È¼Ã", "j¨¬ j¨¬"));
+        ParamCache.LIST.add(new Yijing("64", "Î´¼Ã", "w¨¨i j¨¬"));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ArrayList<Yijing> copy() {
+        return (ArrayList<Yijing>) ParamCache.LIST.clone();
+    }
+
+    public static Yijing find(final String seqId) {
+        for (final Yijing yijing : ParamCache.LIST) {
+            if (seqId.equals(yijing.getSequence())) {
+                return yijing;
+            }
+        }
+        return null;
+    }
+}
